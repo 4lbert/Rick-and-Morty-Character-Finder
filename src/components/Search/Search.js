9f65a0a2d414840aeffb32ae8c6348ca0dbfcd5f
Albert Krzymowski <albertkrzymowski@gmail.com>
2020-03-29 23:47:30 +0200

    Add result selection logic

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -9,6 +9,7 @@ class Search extends React.Component {
     this.state = {
       query: window.location.pathname.split('/')[1],
       characters: [],
+      selected: [],
     };
 
     this.textInput = React.createRef();
@@ -39,12 +40,34 @@ class Search extends React.Component {
       <div className="Search">
         <input type="text" className="TextInput" ref={this.textInput} onChange={this.handleInputChange.bind(this)} />
         <div className="SearchResultContainer">
-          {this.state.characters.map(character =>
-            <div className="SearchResult" key={character.id}>{character.name}</div>
-          )}
+          {this.state.characters.map(character => {
+            const selected = this.state.selected.includes(character.id);
+            return (
+              <div
+                className={`SearchResult ${selected ? 'selected' : ''}`}
+                onClick={() => this.toggleCharacter(character.id)}
+                key={character.id}
+              >
+                {character.name}
+              </div>
+            );
+          })}
         </div>
       </div>
     );
+  }
+
+  toggleCharacter(id) {
+    const index = this.state.selected.indexOf(id);
+    if (index > -1) {
+      let newSelection = this.state.selected;
+      newSelection.splice(index, 1);
+      this.setState({ selected: newSelection });
+    } else {
+      const newSelection = this.state.selected;
+      newSelection.push(id);
+      this.setState({ selected: newSelection });
+    }
   }
 
   /**
